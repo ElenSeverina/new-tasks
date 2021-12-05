c06b7ba40e92774acc71594961b7495373e2f8f8
Elen <elen.garcia.work@gmail.com>
2021-12-05 18:22:50 +0200

    modified interval

--- interval.js
+++ interval.js
@@ -1,20 +1,3 @@
-// function printArray(array, timeout) {
-//   for (var i = 0; i < array.length; i += 1) {//и поменять тут
-//       //добавлять код тут
-//       setTimeout(() => {
-//           console.log(array[i]);
-//       }, timeout);
-//       // и добавлять код тут
-//   }
-// }
-
-// printArray([1, 2, 3], 1000);
-
-// разобраться почему 3 раза выводится undefined
-// 2 способа как поправить код, так что-бы элементы выводились правильно
-// замыкания, var/let/const какая между ними разница
-
-
 //1)
 function printArray(array, timeout) {
     for (var i = 0; i < array.length; i += 1) {
